d3aaea33bf5590c6d0813a92509513177c96872d
Alexander <alexbez@users.noreply.github.com>
2023-07-10 11:16:32 +0300

    Update prepare-router.sh

--- prepare-router.sh
+++ prepare-router.sh
@@ -1,11 +1,45 @@
 #/bin/sh
 set -eu
+echo "=========================================================="
+echo "                 ROTER PREPARATION SCRIPT"
+echo "=========================================================="
 
-if [ "$EUID" -ne 0 ]
-then echo "Please run as root"
-exit
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
 fi
 
-echo "========================================================================"
-echo "           Preparing this computer to work as a NAT router"
-echo "========================================================================"
+ifconfig eth0 192.168.0.1
+echo "Statis IP address 192.168.0.1 set for eth0"
+
+systemctl start isc-dhcp-server.service
+systemctl status isc-dhcp-server.service
+echo "DHCP server started for eth0"
+
+sed -i -e 's/127.0.0.53/8.8.8.8/' /etc/resolv.conf
+echo "Set DNS 8.8.8.8 to resolv.conf"
+
+iptables -F
+iptables -X
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -P INPUT ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -P FORWARD ACCEPT
+echo "iptables cleaned OK"
+
+iptables -A FORWARD -j ACCEPT
+echo "Port forwarding allowed OK"
+
+iptables -t nat -A POSTROUTING -j MASQUERADE
+echo "NAT allowed OK"
+
+#iptables -L
+route
+echo ""
+ifconfig wlan0 | grep "inet" | awk '{print $2}'
+echo ""
+ip route show
+echo "Script completed"
